77f561a5bf7188c3931c3491aae53f5ba1fa1299
MindIssawara <kmind46@gmail.com>
2022-12-23 15:38:21 +0700

    lab9_3 completed

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,40 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string ch;
+    int age,cost,h;
+    cout<<"Enter your age: ";
+    cin>>age;
+    if(age<=25){
+        cout << "Enter your height: ";
+        cin>>h;
+        if(h<100){
+            ch = "Chopper";
+        }else if(h<180){
+            ch = "Usopp";
+        } else{
+            cout<<"Enter your bounty: ";
+            cin>>cost;
+            if(cost>1100000000){
+                ch = "Zoro";
+            }else{
+                ch = "Sanji";
+            }
+        
+        }
+
+    }else if(age<=60){
+        cout<<"Enter your bounty: ";
+        cin>>cost;
+        if(cost<=500000000){
+            ch = "Franky";
+        } else{
+            ch = "Jinbe";
+        }
+    }else {
+        ch = "Brook";
+    }
+    cout<<"Your character = "<<ch;
+}
